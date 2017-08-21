b606349c1431c3e0baab86f950499906340d1705
김건호의 첫번쨰 git <cnzkl6948@naver.com>
2017-08-21 10:11:33 +0900

    ㅁㄴㅇ

--- juyeong/src/main/java/a/a/a/HomeController.java
+++ juyeong/src/main/java/a/a/a/HomeController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.
- */
+ * Handles requests for the application hoe page.
+ */¤·
 @Controller
 public class HomeController {
 	
@@ -26,6 +26,12 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
 	}
